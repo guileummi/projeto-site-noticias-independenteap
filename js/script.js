291b35a0c24517d2b456472f1b8b6fe86af440a1
GUILHERME <111024179+guileummi@users.noreply.github.com>
2026-03-07 01:28:05 -0300

    ajustes no css

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const noticias = [
     {
-        titulo: 'Independente vence clássico',
-        texto: 'O Independente venceu o clássico por 2x0.',
+        titulo: 'Novo reforço anunciado',
+        texto: 'O defensor Sandro Quimarães chega ao clube para somar',
         imagem: '/assets/imagens/foto1.png'
     },
     {
         titulo: 'Novo reforço anunciado',
-        texto: 'O clube anunciou um novo atacante.',
+        texto: 'O lateral Junior Morando chega para se titular',
         imagem: '/assets/imagens/foto1.png'
     },
     {
@@ -16,22 +16,22 @@ const noticias = [
     },
     {
         titulo: 'Base em destaque',
-        texto: 'Sub-20 vence amistoso.',
+        texto: 'Sub-20 vence amistoso contra o flamengo por 12 x 8',
         imagem: '/assets/imagens/foto1.png'
     },
     {
-        titulo: 'Treino da semana',
-        texto: 'Equipe focada no próximo jogo.',
+        titulo: 'Técnico sobre pressão?',
+        texto: 'Equipe não vence a 5 jogos',
         imagem: '/assets/imagens/foto1.png'
     },
     {
-        titulo: 'Treino da semana',
-        texto: 'Equipe focada no próximo jogo.',
+        titulo: 'Jogadores sem raça',
+        texto: 'Torcida xinga os jogadores os chamando de merdas',
         imagem: '/assets/imagens/foto1.png'
     },
     {
-        titulo: 'Treino da semana',
-        texto: 'Equipe focada no próximo jogo.',
+        titulo: 'Reforço de peso?',
+        texto: 'Anunciado o atacante Aleison (40 anos) que chega do rival Trem',
         imagem: '/assets/imagens/foto1.png'
     },
 ]
